88c02752eba6db31c2f3473ef8bad0c6cb974452
Macbook <macbook@MacBook-Pro-de-Juan.local>
2025-06-26 17:52:39 -0300

    Corrige Authorization com backticks e sem caracteres invisíveis

--- index.js
+++ index.js
@@ -48,4 +48,4 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(⁠ Servidor rodando na porta ${PORT} ⁠));
+app.listen(PORT, () => console.log(⁠`ervidor rodando na porta ${PORT} `));
