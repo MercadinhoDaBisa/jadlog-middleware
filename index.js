a6b941d37b2fb734f09a9265073b495c49349fdb
MercadinhoDaBisa <mercadinhodabisa@gmail.com>
2025-07-23 16:14:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/cotacao', async (req, res) => {
       cepdes: cepDestino,
       frap: null,
       peso: pesoTotal,
-      cnpj: process.env.JADLOG_CNPJ,
+      cnpj: "59554346000184",
       conta: process.env.CONTA_CORRENTE || null,
       contrato: null,
       modalidade: modalidade, // Usando a modalidade do loop
